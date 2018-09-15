69582c7a20ef21bc01d2c43d046ffea156258881
uabalabadubdub <42066130+uabalabadubdub@users.noreply.github.com>
2018-09-15 19:07:22 +0200

    Add files via upload
    Hidden key dictionary template

---
+++ hiddenCoinMarketCap_public.py
@@ -0,0 +1,7 @@
+# Keep this file separate
+
+# https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest
+# Create new App and get the Key string
+
+def headers():
+    return {'X-CMC_PRO_API_KEY': 'YOUR KEY INHERE'}
